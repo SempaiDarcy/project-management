6329eb966f8558b0280a5e1f096bba459bab915c
SempaiDarcy <mr_darcy_1@mail.ru>
2024-10-04 02:58:03 +0500

    feat: added Sidebar component with logo and team info, updated DashboardWrapper for integration

--- src/app/dashboard-wrapper.tsx
+++ src/app/dashboard-wrapper.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from 'react';
 import { Navbar } from '@/app/components/navbar';
+import { Sidebar } from '@/app/components/sidebar';
 
 export const DashboardWrapper = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-      sidebar
+      <Sidebar />
       <main
         className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`}
       >
